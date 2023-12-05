fb921cd1e97e8735ef352273d1b7bd796184e327
ziabric <73173019+ziabric@users.noreply.github.com>
2023-12-05 11:34:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=res.get("output"))
  
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6642563075:AAEN0YXFCpaN8CloZpj9itqiO6pwO3HoLqA').build()
+    application = ApplicationBuilder().token('').build()
  
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
